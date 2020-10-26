6ff94ae072aa74f21574495d8ce2fe65ae864d15
Tu Dinh <tudinhacoustic@gmail.com>
2020-10-26 22:08:17 +0700

    Add Bcrypt

--- src/api/controllers/users.go
+++ src/api/controllers/users.go
@@ -1,6 +1,16 @@
 package controllers
 
-import "net/http"
+import (
+	"api/database"
+	"api/models"
+	"api/repository"
+	"api/repository/crud"
+	"api/responses"
+	"encoding/json"
+	"fmt"
+	"io/ioutil"
+	"net/http"
+)
 
 func GetUsers(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("List Users"))
@@ -11,7 +21,35 @@ func GetUser(w http.ResponseWriter, r *http.Request) {
 }
 
 func CreateUser(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("Create Users"))
+	// w.Write([]byte("Create Users"))
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+	user := models.User{}
+	err = json.Unmarshal(body, &user)
+	if err != nil {
+		responses.ERROR(w, http.StatusUnprocessableEntity, err)
+		return
+	}
+	db, err := database.Connect()
+	if err != nil {
+		responses.ERROR(w, http.StatusInternalServerError, err)
+		return
+	}
+
+	repo := crud.NewRepositoryUsersCRUD(db)
+	fmt.Println("REPO", repo)
+	func(usersRepository repository.UserRepository) {
+		user, err := usersRepository.Save(user)
+		if err != nil {
+			responses.ERROR(w, http.StatusUnprocessableEntity, err)
+			return
+		}
+		w.Header().Set("Location", fmt.Sprintf("%s%s%d", r.Host, r.RequestURI, user.ID))
+		responses.JSON(w, http.StatusCreated, user)
+	}(repo)
 }
 
 func UpdateUser(w http.ResponseWriter, r *http.Request) {
---
+++ src/api/repository/crud/repository_users_crud.go
@@ -0,0 +1,33 @@
+package crud
+
+import (
+	"api/models"
+	"api/utils/channels"
+
+	"github.com/jinzhu/gorm"
+)
+
+type repositoryUsersCRUD struct {
+	db *gorm.DB
+}
+
+func NewRepositoryUsersCRUD(db *gorm.DB) *repositoryUsersCRUD {
+	return &repositoryUsersCRUD{db}
+}
+
+func (r *repositoryUsersCRUD) Save(user models.User) (models.User, error) {
+	var err error
+	done := make(chan bool)
+	go func(ch chan<- bool) {
+		err = r.db.Debug().Model(&models.User{}).Create(&user).Error
+		if err != nil {
+			ch <- false
+			return
+		}
+		ch <- true
+	}(done)
+	if channels.OK(done) {
+		return user, nil
+	}
+	return models.User{}, err
+}
